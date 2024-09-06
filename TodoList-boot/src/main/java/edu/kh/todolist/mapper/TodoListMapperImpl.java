656955a1ad5448c4f1ad526267e20eba4364fa81
MAX-G <go1991woo@naver.com>
2024-09-06 17:24:06 +0900

    240906_Framework 사용자 조회_TodoList 수업자료 커밋

---
+++ TodoList-boot/src/main/java/edu/kh/todolist/mapper/TodoListMapperImpl.java
@@ -0,0 +1,8 @@
+package edu.kh.todolist.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper // 상속받은 클래스 생성 후 Bean으로 등록
+public class TodoListMapperImpl {
+
+}
--- demoProject3/src/main/java/edu/kh/demo/service/UserServiceImpl.java
+++ demoProject3/src/main/java/edu/kh/demo/service/UserServiceImpl.java
@@ -1,31 +1,75 @@
 package edu.kh.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import edu.kh.demo.dto.UserDto;
 import edu.kh.demo.mapper.TestMapper;
 
-
 // @Service
-// - Service 역할(비즈니스 로직 처리) 임 명시
-// - Bean 등록 (== Spring이 관리하는 객체 == IOC)
+// - Service 역할(비즈니스 로직 처리)임 명시
+// - Bean 등록 (== Spring이 관리하는 객체 == IOC )
 
-@Service
-public class UserServiceImpl implements UserService{
+@Service 
+public class UserServiceImpl implements UserService {
 
-	/*	@AutoWired
+	/* @Autowired
 	 * - 등록된 Bean 중에서
-	 * 	 자료형이 같은 Bean 을 얻어와 필드에 대입
-	 * 	 == DI(의존성 주입)
+	 *   자료형이 같은 Bean을 얻어와 필드에 대입
+	 *   == DI(의존성 주입)
 	 */
-	
 	@Autowired
 	private TestMapper mapper;
-
+	
+	
+	// 사용자 이름 조회 
 	@Override
 	public String selectUserName(int userNo) {
 		return mapper.selectUserName(userNo);
 	}
+	
+	
+	// 사용자 전체 조회
+	@Override
+	public List<UserDto> selectAll() {
+		// mapper : 의존성 주입(DI) 받은 TestMapper를
+		//				 상속 받아 구현된 클래스로 만들어진 Bean(객체)
+		return mapper.selectAll();
+	}
+	
+	
+	// userNo가 일치하는 사용자 조회
+	@Override
+	public UserDto selectUser(int userNo) {
+		return mapper.selectUser(userNo);
+	}
+	
+	// 사용자 정보 수정(DML)
+	// -> DML 수행하면 트랜잭션 제어 처리
+	// @Transactional : 
+	// - 해당 메서드 수행 중 RuntimeException 발생 시 ROLLBACK 수행
+	// - 예외가 발생하지 않으면 메서드 종료 후 COMMIT 수행
+	@Transactional
+	@Override
+	public int updateUser(UserDto user) {
+		return mapper.updateUser(user);
+	}
+
+	@Transactional // 예외 발생 시 롤백, 아니면 메서드 종료 시 커밋 
+	@Override
+	public int deleteUser(int userNo) {
+		return mapper.deleteUser(userNo);
+	}
+
+	@Transactional
+	@Override
+	public int insertUser(UserDto user) {
+		return mapper.insertUser(user);
+	}
+	
 	
 	
 }
