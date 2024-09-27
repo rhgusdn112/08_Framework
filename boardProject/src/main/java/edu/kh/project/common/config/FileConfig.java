227e02ad521c6f7d6ff49725036ee9ebcdaa451a
MAX-G <go1991woo@naver.com>
2024-09-27 16:41:05 +0900

    240927 수업자료 커밋

--- boardProject/src/main/java/edu/kh/project/common/config/FileConfig.java
+++ boardProject/src/main/java/edu/kh/project/common/config/FileConfig.java
@@ -41,7 +41,20 @@ public class FileConfig implements WebMvcConfigurer {
 	
 	@Value("${my.test.resource-location}")
 	private String testResourceLocation; // 테스트 이미지 요청 시
-										 // 연결될 서버 폴더 경로
+																			 // 연결될 서버 폴더 경로
+	
+	
+	// ----------------------------------------------------------
+	
+	// 프로필 이미지 요청 경로 + 서버 연결 폴더
+	
+	@Value("${my.profile.resource-handler}")
+	private String profileResourceHandler;
+	
+	@Value("${my.profile.resource-location}")
+	private String profileResourceLocation;
+	
+	
 	
 
 	/* MultipartResolver 설정 */
@@ -84,6 +97,13 @@ public class FileConfig implements WebMvcConfigurer {
 		.addResourceLocations(testResourceLocation);
 		//  /images/test/ 로 시작하는 주소로 요청이 있을 경우
 		//  서버 컴퓨터에 C:/uploadFiles/test/ 폴더로 연결
+		
+		
+		registry
+		.addResourceHandler(profileResourceHandler)
+		.addResourceLocations(profileResourceLocation);
+		//  /images/profile/ 로 시작하는 주소로 요청이 있을 경우
+		//  서버 컴퓨터에 C:/uploadFiles/profile/ 폴더로 연결
 		
 	}
 	
